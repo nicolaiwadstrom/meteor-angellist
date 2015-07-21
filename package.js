1754f0523b250d198625c7b6f519caa66594f109
Qiming Fang <qf26@cornell.edu>
2015-07-21 14:02:17 -0700

    change package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'qimingfang:meteor-angellist',
+  name: 'fang:meteor-angellist',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'AngelList API for Meteor',
